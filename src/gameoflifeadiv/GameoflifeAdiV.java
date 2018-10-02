59fd1e926ad0a85b95e93f2c22d3e5ed0ab1c0f7
Adi Venkatesh <Adi Venkatesh@192.168.0.20>
2018-10-01 22:14:30 -0400

    starting good copy for project

--- src/gameoflifeadiv/GameoflifeAdiV.java
+++ src/gameoflifeadiv/GameoflifeAdiV.java
@@ -15,7 +15,7 @@ public class GameoflifeAdiV {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Testing Github for good copy
     }
     
 }
